c0a0a72315a0f565183925f55197de96374b4eb5
Pablo Ariel Campos <pabloarielcampos@gmail.com>
2021-11-10 19:56:27 -0300

    [ADD] Ejercicio concluido. Resta resolver el problema del input de hobbie. El programa se salta la línea y no me deja introducir datos

---
+++ src/Ejercicio_Unidad_2.java
@@ -0,0 +1,40 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+import java.util.Scanner;
+
+public class Ejercicio_Unidad_2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hola, voy a pedirte unos datos para guardar en mi sistema. ¿Podrías empezar escribiendo tu nombre?");
+
+        String name = sc.nextLine();
+
+        System.out.println("Y tu apellido, por supuesto: ");
+
+        String lastName = sc.nextLine();
+
+        System.out.println("Vamos bien, ahora quisiera saber tu edad: ");
+
+        int age = sc.nextInt();
+
+        System.out.println("Podrías decirme cuál es tu hobbie: ");
+
+        String hobbie = sc.nextLine();
+
+        System.out.println("¿Cuál editor de código preferido?");
+
+        String editor = sc.nextLine();
+
+
+        System.out.println("Por último, me gustaría saber qué sistema operativo usas:");
+
+        String opSystem = sc.nextLine();
+
+        System.out.println("¡Muy bien! Te llamás " + name + " " + lastName + " " + " y tenés " + age + " años. \n Tu hobbie es " + hobbie
+        + " y tu editor de código favorito es " + editor + " Por último, tu sistema operativo es " + opSystem + ".\n ¡Muchas gracias por responder!");
+
+    }
+}
